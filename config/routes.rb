90ddc8c2a225333bef52a8ce08d6d71557fd8359
kensuke1003 <kensukeseven1003@gmail.com>
2021-03-29 02:07:04 +0000

    last

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about", to: 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
+  
+  #ネスト構造
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
